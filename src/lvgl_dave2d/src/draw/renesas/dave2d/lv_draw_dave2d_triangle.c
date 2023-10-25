e886a970b273ce53ef50c056e717ed7b565e2b3b
Jeremy Baker <jeremy.baker@renesas.com>
2023-10-25 19:33:02 +0100

    Update to FSP 5.0.0

--- src/lvgl_dave2d/src/draw/renesas/dave2d/lv_draw_dave2d_triangle.c
+++ src/lvgl_dave2d/src/draw/renesas/dave2d/lv_draw_dave2d_triangle.c
@@ -1,16 +1,13 @@
 #include <renesas/dave2d/lv_draw_dave2d.h>
 #if LV_USE_DRAW_DAVE2D
 
+
 void lv_draw_dave2d_triangle(lv_draw_dave2d_unit_t * draw_dave2d_unit, const lv_draw_triangle_dsc_t * dsc)
 {
     lv_area_t clipped_area;
     d2_u32      flags = 0;
-    lv_color_t  colour;
-    lv_grad_t * grad = NULL;
-    d2_color * p_texture_buf = NULL;
-
     d2_u8 current_alpha_mode = 0;
-    d2_alpha current_alpha_value = 0;
+
 
 #if CHECK_RENDERING_TO_VISIBLE_FB
     if ((R_GLCDC->GR[0].FLM2 == (uint32_t)draw_dave2d_unit->base_unit.target_layer->buf))
@@ -22,7 +19,7 @@ void lv_draw_dave2d_triangle(lv_draw_dave2d_unit_t * draw_dave2d_unit, const lv_
     tri_area.x1 = LV_MIN3(dsc->p[0].x, dsc->p[1].x, dsc->p[2].x);
     tri_area.y1 = LV_MIN3(dsc->p[0].y, dsc->p[1].y, dsc->p[2].y);
     tri_area.x2 = LV_MAX3(dsc->p[0].x, dsc->p[1].x, dsc->p[2].x);
-    tri_area.y2 = LV_MAX3(dsc->p[0].y, dsc->p[1].y, dsc->p[2].y);
+    tri_area.y2 = LV_MAX3(dsc->p[0].y, dsc->p[1].y, dsc->p[2].y) - 2; /* TODO - Why does the triangle overlap in the wigets demo, Analytics screen, top graph? */
 
 
     if(!_lv_area_intersect(&clipped_area, &tri_area, draw_dave2d_unit->base_unit.clip_area)) return;
@@ -83,11 +80,6 @@ void lv_draw_dave2d_triangle(lv_draw_dave2d_unit_t * draw_dave2d_unit, const lv_
 
     if (LV_GRAD_DIR_NONE != dsc->bg_grad.dir)
     {
-        grad = lv_gradient_get(&dsc->bg_grad, lv_area_get_width(&tri_area), lv_area_get_height(&tri_area));
-
-
-        lv_coord_t area_w = lv_area_get_width(&clipped_area);
-        lv_coord_t area_h = lv_area_get_height(&clipped_area);
         float a1;
         float a2;
 
@@ -125,7 +117,7 @@ void lv_draw_dave2d_triangle(lv_draw_dave2d_unit_t * draw_dave2d_unit, const lv_
               y0_i = (int16_t)y0;
               y3_i = (int16_t)y3;
 
-              d2_setalphagradient(draw_dave2d_unit->d2_handle, 0, D2_FIX4(clipped_area.x1),  D2_FIX4(y0_i), D2_FIX4(0), D2_FIX4(y3_i - y0_i) );
+              d2_setalphagradient(draw_dave2d_unit->d2_handle, 0, D2_FIX4(clipped_area.x1),  D2_FIX4(y0_i), D2_FIX4(0), D2_FIX4((int16_t)(y3_i - y0_i)) );
         }
         else if (LV_GRAD_DIR_HOR == dsc->bg_grad.dir)
         {
@@ -134,22 +126,16 @@ void lv_draw_dave2d_triangle(lv_draw_dave2d_unit_t * draw_dave2d_unit, const lv_
         }
 
         current_alpha_mode = d2_getalphamode( draw_dave2d_unit->d2_handle  );
-        current_alpha_value = d2_getalpha(draw_dave2d_unit->d2_handle );
-        d2_setfillmode(draw_dave2d_unit->d2_handle, d2_fm_color);
+        d2_setfillmode(draw_dave2d_unit->d2_handle, d2_fm_color); //default
         d2_setcolor(draw_dave2d_unit->d2_handle, 0, lv_draw_dave2d_lv_colour_to_d2_colour(dsc->bg_grad.stops[0].color));
         d2_setalphamode(draw_dave2d_unit->d2_handle, d2_am_gradient1 );
-
-
-
-
     }
     else
     {
-        colour = dsc->bg_color;
         d2_setfillmode(  draw_dave2d_unit->d2_handle, d2_fm_color ); //default
         d2_setalpha( draw_dave2d_unit->d2_handle, dsc->bg_opa  );
 
-        d2_setcolor(draw_dave2d_unit->d2_handle, 0, lv_draw_dave2d_lv_colour_to_d2_colour(colour));
+        d2_setcolor(draw_dave2d_unit->d2_handle, 0, lv_draw_dave2d_lv_colour_to_d2_colour(dsc->bg_color));
 
     }
 
@@ -180,26 +166,9 @@ void lv_draw_dave2d_triangle(lv_draw_dave2d_unit_t * draw_dave2d_unit, const lv_
     d2_executerenderbuffer(draw_dave2d_unit->d2_handle, draw_dave2d_unit->renderbuffer, 0);
     d2_flushframe(draw_dave2d_unit->d2_handle);
 
-    if(grad) {
-        lv_gradient_cleanup(grad);
-    }
-
-    if (p_texture_buf)
-    {
-        lv_free(p_texture_buf);
-    }
-
     if (LV_GRAD_DIR_NONE != dsc->bg_grad.dir)
     {
-        d2_setfillmode(  draw_dave2d_unit->d2_handle, d2_fm_color ); //default
-        d2_settextureoperation( draw_dave2d_unit->d2_handle,
-                d2_to_one, d2_to_copy, d2_to_copy, d2_to_copy ); //Defaults
-        d2_setalphablendmode(   draw_dave2d_unit->d2_handle,
-                d2_bm_one, d2_bm_zero  ); //Defaults
-        d2_setblendmode(    draw_dave2d_unit->d2_handle,
-                d2_bm_alpha , d2_bm_one_minus_alpha  ); //Defaults
         d2_setalphamode(draw_dave2d_unit->d2_handle, current_alpha_mode);
-        d2_setalpha(draw_dave2d_unit->d2_handle, current_alpha_value);
     }
 
 #if LV_USE_OS
