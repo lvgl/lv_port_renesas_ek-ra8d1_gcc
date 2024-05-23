becseya/lv_port_renesas_ek-ra8d1

Move GLCDC driver to LVGL lib

lv_ek_ra8d1/src/board_init.c
@@ -1,0 +1,58 @@
+#include "board_init.h"
+
+#include "lvgl.h"
+#include "port/lv_port_indev.h"
+#include "lvgl/src/drivers/display/renesas_glcdc/lv_renesas_glcdc.h"
+#include "common_data.h"
+
+#include "LVGL_thread.h"
+#include "touch_GT911.h"
+
+static void touch_init(void)
+{
+    fsp_err_t err;
+
+    /* Need to initialise the Touch Controller before the LCD, as only a Single Reset line shared between them */
+    err = R_IIC_MASTER_Open(&g_i2c_master1_ctrl, &g_i2c_master1_cfg);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+
+    err = R_ICU_ExternalIrqOpen(&g_external_irq3_ctrl, &g_external_irq3_cfg);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+
+    err = init_ts(&g_i2c_master1_ctrl);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+
+    err = enable_ts(&g_i2c_master1_ctrl, &g_external_irq3_ctrl);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+}
+
+void board_init(void)
+{
+    lv_init();
+
+    /* Need to initialise the Touch Controller before the LCD, as only a Single Reset line shared between them */
+    touch_init();
+
+#if 1
+    lv_display_t * disp = lv_renesas_glcdc_create_direct();
+#else
+    static lv_color_t partial_draw_buf[DISPLAY_HSIZE_INPUT0 * DISPLAY_VSIZE_INPUT0 / 10] BSP_PLACE_IN_SECTION(".sdram") BSP_ALIGN_VARIABLE(1024);
+
+    lv_display_t * disp = lv_renesas_glcdc_create_partial(partial_draw_buf, NULL, sizeof(partial_draw_buf));
+#endif
+    lv_display_set_default(disp);
+
+    lv_port_indev_init();
+}

lvgl/lv_port_renesas_ek-ra8d1_gcc

src/board_init.c
@@ -1,0 +1,62 @@
+#include "board_init.h"
+
+#include "lvgl.h"
+#include "port/lv_port_indev.h"
+#include "lvgl/src/drivers/display/renesas_glcdc/lv_renesas_glcdc.h"
+#include "common_data.h"
+
+#include "LVGL_thread.h"
+#include "touch_GT911.h"
+
+static void touch_init(void)
+{
+    fsp_err_t err;
+
+    /* Need to initialise the Touch Controller before the LCD, as only a Single Reset line shared between them */
+    err = R_IIC_MASTER_Open(&g_i2c_master1_ctrl, &g_i2c_master1_cfg);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+
+    err = R_ICU_ExternalIrqOpen(&g_external_irq3_ctrl, &g_external_irq3_cfg);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+
+    err = init_ts(&g_i2c_master1_ctrl);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+
+    err = enable_ts(&g_i2c_master1_ctrl, &g_external_irq3_ctrl);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0); //TODO: Better error handling
+    }
+}
+
+void board_init(void)
+{
+    lv_init();
+
+    /* Need to initialise the Touch Controller before the LCD, as only a Single Reset line shared between them */
+    touch_init();
+
+#if 1
+    lv_display_t * disp = lv_renesas_glcdc_direct_create();
+#else
+    static lv_color_t partial_draw_buf[DISPLAY_HSIZE_INPUT0 * DISPLAY_VSIZE_INPUT0 / 10] BSP_PLACE_IN_SECTION(".sdram") BSP_ALIGN_VARIABLE(1024);
+
+    lv_display_t * disp = lv_renesas_glcdc_partial_create(partial_draw_buf, NULL, sizeof(partial_draw_buf));
+#endif
+
+    lv_display_set_default(disp);
+
+    /* Enable the backlight */
+    R_IOPORT_PinWrite(&g_ioport_ctrl, DISP_BLEN, BSP_IO_LEVEL_HIGH);
+
+    lv_port_indev_init();
+}