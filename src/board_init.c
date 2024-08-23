bc33b35f8a226e9bab0f87dea38dabf1daa1a871
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-08-23 12:27:26 +0200

    add manifest.json

--- src/board_init.c
+++ src/board_init.c
@@ -8,6 +8,8 @@
 #include "LVGL_thread.h"
 #include "touch_GT911.h"
 
+#define DIRECT_MODE 1
+
 static void touch_init(void)
 {
     fsp_err_t err;
@@ -43,10 +45,10 @@ void board_init(void)
     /* Need to initialise the Touch Controller before the LCD, as only a Single Reset line shared between them */
     touch_init();
 
-#if 1
+#if DIRECT_MODE
     lv_display_t * disp = lv_renesas_glcdc_direct_create();
 #else
-    static lv_color_t partial_draw_buf[DISPLAY_HSIZE_INPUT0 * DISPLAY_VSIZE_INPUT0 / 10]  BSP_ALIGN_VARIABLE(1024);
+    static lv_color_t partial_draw_buf[DISPLAY_HSIZE_INPUT0 * DISPLAY_VSIZE_INPUT0 / 10] BSP_ALIGN_VARIABLE(1024);
 
     lv_display_t * disp = lv_renesas_glcdc_partial_create(partial_draw_buf, NULL, sizeof(partial_draw_buf));
 #endif
@@ -58,3 +60,4 @@ void board_init(void)
 
     lv_port_indev_init();
 }
+
