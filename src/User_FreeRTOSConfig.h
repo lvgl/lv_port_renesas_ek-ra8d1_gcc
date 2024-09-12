5781e0888e9906b6059b1448005f56172c8f66ec
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-09-12 12:55:53 +0200

    update LVGL

--- src/User_FreeRTOSConfig.h
+++ src/User_FreeRTOSConfig.h
@@ -2,11 +2,14 @@
 #define USER_FREERTOSCONFIG_H_
 
 #define configUSE_TRACE_FACILITY 1
+#define configUSE_RECURSIVE_MUTEXES 1
 
 void lv_freertos_task_switch_in(const char * name);
 void lv_freertos_task_switch_out(void);
 
 #define traceTASK_SWITCHED_IN()   lv_freertos_task_switch_in(pxCurrentTCB->pcTaskName);
 #define traceTASK_SWITCHED_OUT()  lv_freertos_task_switch_out();
+
+#define configUSE_RECURSIVE_MUTEXES 1
 
 #endif /* USER_FREERTOSCONFIG_H_ */
