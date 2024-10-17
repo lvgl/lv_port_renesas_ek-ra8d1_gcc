f38c18b4abe8dbf08901c5e700bf8575bef567ab
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-10-17 11:04:21 +0200

    fix(os): fix build error without FreeRTOS

--- src/User_FreeRTOSConfig.h
+++ src/User_FreeRTOSConfig.h
@@ -1,6 +1,10 @@
 #ifndef USER_FREERTOSCONFIG_H_
 #define USER_FREERTOSCONFIG_H_
 
+#include  "lvgl/src/lv_conf_internal.h"
+
+#if LV_USE_OS == LV_OS_FREERTOS
+
 #define configUSE_TRACE_FACILITY 1
 #define configUSE_RECURSIVE_MUTEXES 1
 
@@ -11,5 +15,6 @@ void lv_freertos_task_switch_out(void);
 #define traceTASK_SWITCHED_OUT()  lv_freertos_task_switch_out();
 
 #define configUSE_RECURSIVE_MUTEXES 1
+#endif
 
 #endif /* USER_FREERTOSCONFIG_H_ */
